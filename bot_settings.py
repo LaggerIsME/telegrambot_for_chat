da7086191d8f55932464d9a2c3444ccddcc6bf2e
LaggerIsME <pythondelay@gmail.com>
2023-03-09 04:57:17 +0600

    Added TOKEN as env.variable instead of token.json. Moreover, I added that while MongoDB container is starting, it use mongorestore.sh that restore all data from backup

--- bot_settings.py
+++ bot_settings.py
@@ -1,27 +1,11 @@
-import asyncio
-import json
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from ai_for_chatting.ai_config import ReplicaGenerator
 from database import redis
-
-
-# Получить токен бота
-def get_token(path: str = 'token.json'):
-    try:
-        # Открыть файл с токеном
-        with open(path, 'r') as read_file:
-            data = json.load(read_file)
-        # Достать его из словаря
-        token = data['token']
-        return token
-    except:
-        print("Неправильно указан путь к файлу с токеном, либо сам файл неправильного формата")
-        return None
-
+import os
 
 # Настройки бота
-token = get_token()
+token = os.environ.get('BOT_TOKEN')
 # Создание бота
 bot = Bot(token=token)
 # Обрабатыватель сообщений Telegram
---
+++ database/mongorestore.sh
@@ -0,0 +1,1 @@
+mongorestore -d "filter" /database/backup
